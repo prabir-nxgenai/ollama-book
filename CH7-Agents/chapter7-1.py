d408519ab06b5f986d8d522d4eadb60f7becc95c
prabir-nxgenai <prabir.guha@nxgen.ai>
2025-04-24 11:13:37 -0400

    Add DuckDuckGo search functionality and refactor agent initialization in chapter7-1.py; remove unused files chapter7-2.py and chapter7-3.py

--- CH7-Agents/chapter7-1.py
+++ CH7-Agents/chapter7-1.py
@@ -1,20 +1,45 @@
 from langchain.tools import Tool
 from langchain.agents import initialize_agent
-from langchain_ollama import OllamaLLM
+from langchain_community.chat_models import ChatOllama
+from duckduckgo_search import DDGS
 
-# Define a simple function
+# Define a function to get the current time
 def get_time(_):
     from datetime import datetime
     return datetime.now().strftime("%Y-%m-%d %H:%M:%S")
 
-# Register function as a tool
-time_tool = Tool(name="current_time", func=get_time, description="Returns the current time")
+#Define a function to search the internet
+def duckduckgo_search(query: str) -> str:
+    try:
+        with DDGS() as ddgs:
+            results = ddgs.text(query, max_results=3)
+            for result in results:
+                snippet = result.get("body") or result.get("snippet")
+                if snippet:
+                    return snippet
+            return "No relevant results found."
+    except Exception as e:
+        return f"Error during DuckDuckGo search: {str(e)}"
+    
+# Register the two functions as a tools
+time_tool = Tool(
+        name="current_time", 
+        func=get_time, 
+        description="Returns the current time"
+)
+duck_tool = Tool(
+    name="duckduckgo_search",
+    func=duckduckgo_search,
+    description="Searches the Internet. Input should be a search query string."
+)
+# Initialize the Ollama model
+llm = ChatOllama(model="llama3.1", base_url="http://localhost:11434")
 
-# Initialize an agent
-llm = OllamaLLM(model="llama3.1")
-agent = initialize_agent([time_tool], llm, agent="zero-shot-react-description", handle_parsing_errors=True, verbose=True)
+# Initialize the agent
+agent = initialize_agent([time_tool, duck_tool], llm, agent="zero-shot-react-description", handle_parsing_errors=True, verbose=True)
 
 
-# Execute a query that triggers the function
-print(agent.invoke("What is the current time?"))
+# Execute queries that trigger time and search tools by the agent
+print(agent.invoke("Question 1: What is the current time? Provide answer in YYYY-MM-DD HH:MM:SS format"))
+print(agent.invoke("Question 1: What is the closest planet to the sun?"))
 
